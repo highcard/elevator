7a8b5c87444e2ea890df543c9e1ec028842d35e6
hC <toby.m.hall@gmail.com>
2014-02-21 02:27:34 -0500

    added floor button list

--- elevator.py
+++ elevator.py
@@ -1,7 +1,7 @@
 from types import *
 
 class Elevator(object):
-	def __init__(self, min_floor, max_floor, cur_floor, direction):
+	def __init__(self, min_floor, max_floor, cur_floor, default_floor, direction):
 		## Type Assertions
 		assert type(min_floor) is IntType, "is not an integer: %r" % min_floor
 		assert type(max_floor) is IntType, 'is not an integer: %r' % max_floor
@@ -11,20 +11,21 @@ class Elevator(object):
 		#Bounds Assertions
 		assert min_floor < max_floor, 'invalid min_floor and max_floor values. min: %r, max: %r' % (min_floor, max_floor)
 		assert cur_floor >= min_floor and cur_floor <= max_floor, 'cur_floor is out of range: %r' % cur_floor
-		assert direction == 0 or direction == 1, 'invalid direction: %r' % direction	
 		
 		#Initialize class attributes
 		self.cur_floor = cur_floor
 		self.min_floor = min_floor
 		self.max_floor = max_floor
+		self.default_floor = default_floor
 		self.direction = direction
 		self.moving = False
-		self.floor_list = [(f for f in range(min_floor, max_floor), False)]
+		self.floor_list = [False for floor in range(min_floor, max_floor)] #initialize all floors buttons to off
 
 	# prints object attributes
 	def print_stats(self):
 		attrs = vars(self)
-		print ', '.join("%s: %s" % item for item in attrs.items())
+		print "===PRINTING STATS==="
+		print ', \n'.join("%s: %s" % item for item in attrs.items())
 
 	# moves elevator in current direction
 	def move(self):
@@ -39,7 +40,6 @@ class Elevator(object):
 			self.switch_direction()
 		self.cur_floor = self.cur_floor + (1 if up else -1)
 		
-
 	# changes the direction of the elevator True = up / False = down
 	def switch_direction(self):
 		self.direction = not self.direction
@@ -57,34 +57,28 @@ class Elevator(object):
 		self.moving = False
 
 	# resets the floor list
-	def reset(self):
-		self.floor_list = []
+	def reset_floor_list(self):
+		for floor in self.floor_list:
+			floor = False
 
+	# adds a floor button request
 	def press_floor_button(self, floor):
+		self.floor_list[floor] = True
+
+	# removes a floor button request
+	def remove_floor_button(self, floor):
+		self.floor_list[floor] = False
+
+	# check if button is pressed on current floor
+	def check_button(self, floor):
+		return self.floor_list[floor]
 
 
-
-
-
-class Controller(object):
-	def __init___(self):
-		self.min_floor = 0
-		self.max_floor = 10
-		self.default_floor = 0
-		self.default_direction = True
-		self.bank = []
-
-myElevator = Elevator(0, 10, 5, True)
+myElevator = Elevator(0, 10, 1, 1, True)
 myElevator.print_stats()
-myElevator.move()
+myElevator.press_floor_button(1)
 myElevator.print_stats()
-myElevator.move()
-myElevator.print_stats()
-myElevator.switch_direction()
-myElevator.print_stats()
-myElevator.move()
-myElevator.print_stats()
-myElevator.switch_direction()
-myElevator.print_stats()
-myElevator.stop()
-myElevator.print_stats()
+print myElevator.check_button(1)
+print myElevator.check_button(2)
+myElevator.remove_floor_button(1)
+print myElevator.check_button(1)
